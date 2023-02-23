9c7296256da665634d15012375e07384f8928c0e
mercedes-arnwine <80288739+mercedes-arnwine@users.noreply.github.com>
2023-02-22 17:24:30 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = [
+  "Learning How to Code",
+  "Learning React",
+  "Getting Good at Interviewing"
+];
+
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1, 1);
+
+console.log(learning);
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
